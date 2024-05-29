7478d19cb6758e1c20c96dfcdcefa7e6172364c4
chloechiaw <chloechia@gmail.com>
2024-05-29 15:31:45 -0700

    bruh

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -5,22 +5,19 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 #[test]
-fn main() {
+fn test_fill_vec() {
     let vec0 = vec![22, 44, 66];
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0); // Pass in the vector by value
 
-    assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+    vec.push(88); // Add an element
 
-    vec.push(88);
-
-    vec
+    vec // Return the modified vector
 }
