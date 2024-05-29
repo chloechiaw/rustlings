9d85c21c28dc3b476dfc7fcc83fdcf1e4496e946
chloechiaw <chloechia@gmail.com>
2024-05-28 22:08:50 -0700

    adf

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -3,13 +3,16 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {}", answer);
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    return num * num;
 }
+
+// two solutions to this: 
+    // 1. Add a `return` ahead of `num * num;`
+    // 2. remove `;`, make it to be `num * num`
+// remove the semicolon = allows fn to return a value. 
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(number: i32) -> i32 { // function if it takes in a parameter, you also need return type parameter 
+    if number > 40 {
+        return number * 1;
+    } else {
+        return number * 2;
+    }
+}
+
 
 // Don't modify this function!
 #[test]
